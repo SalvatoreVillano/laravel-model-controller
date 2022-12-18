4065f188be302054514c8415e5355941eb73da49
SalvatoreVillano <villano.salvatore9@gmail.com>
2022-12-18 18:00:36 +0100

    creating Model & Controller

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use App\Models\Movie;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    //
+}
